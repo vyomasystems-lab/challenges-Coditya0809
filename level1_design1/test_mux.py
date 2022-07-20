13c75c212d065347b952cfdbeb45213c02d8c8d8
Coditya0809 <adityasai525@gmail.com>
2022-07-20 15:45:10 +0000

    Added hints for self in test module

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -8,3 +8,14 @@ async def test_mux(dut):
     """Test for mux2"""
 
     cocotb.log.info('##### CTB: Develop your test here ########')
+    # Change the below code for testing the Mux module
+    A = 5
+    B = 10
+
+    # input driving
+    dut.a.value = A
+    dut.b.value = B
+
+    await Timer(2, units='ns')
+
+    assert dut.sum.value == A+B, f"Adder result is incorrect: {dut.X.value} != 15"
