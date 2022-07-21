1731d6be9e144de76ecd78880348b793bf19c1d3
Coditya0809 <adityasai525@gmail.com>
2022-07-21 14:37:25 +0000

    Updated Level 1 Design 1 py model file.

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -2,20 +2,19 @@
 
 import cocotb
 from cocotb.triggers import Timer
+import random
 
 @cocotb.test()
 async def test_mux(dut):
     """Test for mux2"""
 
     cocotb.log.info('##### CTB: Develop your test here ########')
-    # Change the below code for testing the Mux module
-    A = 5
-    B = 10
 
     # input driving
-    dut.a.value = A
-    dut.b.value = B
-
+    seeded_rand_value = random.randint(0,3)
+    dut.inp11.value = (1 + seeded_rand_value) % 4
+    # giving dut.inp12 a non-zero value not equal to inp11 to detect error always
+    dut.inp12.value = seeded_rand_value + 2 if (0 == seeded_rand_value) else seeded_rand_value
+    dut.sel.value = 12
     await Timer(2, units='ns')
-
-    assert dut.sum.value == A+B, f"Adder result is incorrect: {dut.X.value} != 15"
+    assert dut.out.value == dut.inp12.value, f"Mux output is incorrect: Expected Output != Model Output : {dut.inp12.value} != {dut.out.value}. Expected Value: {dut.inp12.value}"
