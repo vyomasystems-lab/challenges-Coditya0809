7ec46cc026ced34cf968f93a5829a216e25bb043
Coditya0809 <adityasai525@gmail.com>
2022-07-28 07:57:06 +0000

    Detected the bug in the design with tb

--- level2_design/test_mkbitmanip.py
+++ level2_design/test_mkbitmanip.py
@@ -35,43 +35,44 @@ def run_test(dut):
     ######### CTB : Modify the test to expose the bug #############
 
     err_count = 0
-    for i in range (1):
+    for i in range (1000):
         # input transaction
         mav_putvalue_src1 = random.randint(0,2**32-1)
         mav_putvalue_src2 = random.randint(0,2**32-1)
         mav_putvalue_src3 = random.randint(0,2**32-1)
 
         # intelligently choosing a random instruction
-        mav_putvalue_instr = bin(0)[2:].zfill(32)
+        mav_putvalue_instr = random.getrandbits(32)
+        le = bin(mav_putvalue_instr)[2:].zfill(32)
+
         opcode_seq = [bin(51)[2:].zfill(7),bin(19)[2:].zfill(7)]
         func7_seq = [bin(32)[2:].zfill(7),bin(16)[2:].zfill(7),bin(48)[2:].zfill(7),bin(36)[2:].zfill(7),bin(20)[2:].zfill(7),bin(52)[2:].zfill(7),bin(5)[2:].zfill(7),bin(4)[2:].zfill(7)]
-        func3_seq = [bin(1)[2:].zfill(3),bin(2)[2:].zfill(3),bin(3)[2:].zfill(3),bin(4)[2:].zfill(3),bin(5)[2:].zfill(3),bin(6)[2:].zfill(3),bin(7)[2:].zfill(3)]
+        func3_r_seq = [bin(1)[2:].zfill(3),bin(2)[2:].zfill(3),bin(3)[2:].zfill(3),bin(4)[2:].zfill(3),bin(5)[2:].zfill(3),bin(6)[2:].zfill(3),bin(7)[2:].zfill(3)]
+        func3_imm_seq = ["001","101"]
         func7_imm_seq = [bin(4)[2:].zfill(5),bin(12)[2:].zfill(5),bin(9)[2:].zfill(5),bin(5)[2:].zfill(5),bin(13)[2:].zfill(5)]
-        func7_2bit_seq = ["11","10"]
-        func7_1bit_seq = ["1","0"]
-        func7_fsri_1bit_seq = ["0","1"]
-        func7_imm_SHFL_seq = ["000010"]
-        imm_value_seq = ["1","0"]
-        imm_value_1_seq = [bin(0)[2:].zfill(5),bin(1)[2:].zfill(5),bin(2)[2:].zfill(5),bin(4)[2:].zfill(5),bin(5)[2:].zfill(5),bin(16)[2:].zfill(5),bin(17)[2:].zfill(5),bin(18)[2:].zfill(5),bin(24)[2:].zfill(5),bin(25)[2:].zfill(5),bin(26)[2:].zfill(5)]
+        func7_2bit_seq = ["11","10","00"]
+        #imm_value_1_seq = [bin(0)[2:].zfill(5),bin(1)[2:].zfill(5),bin(2)[2:].zfill(5),bin(4)[2:].zfill(5),bin(5)[2:].zfill(5),bin(16)[2:].zfill(5),bin(17)[2:].zfill(5),bin(18)[2:].zfill(5),bin(24)[2:].zfill(5),bin(25)[2:].zfill(5),bin(26)[2:].zfill(5)]
 
-        le = func7_seq[-3]+"1111111111"+func3_seq[4]+"11111"+opcode_seq[0]
+        # choosing the random sequences
+        opcode = random.choices(opcode_seq,cum_weights = [35, 58],k=1)[0]
+        func7 = random.choice(func7_seq)
+        func3_r = random.choice(func3_r_seq)
+        func3_imm = random.choice(func3_imm_seq)
+        func7_imm = random.choice(func7_imm_seq)
+        func7_2bit = random.choice(func7_2bit_seq)
+        func7_fsri_1bit = random.choice(["0","1"])
 
-        length = 32
-        # le = bin(0)[2:].zfill(length)
-        # le = bin(random.randint(0,2**32-1))[2:].zfill(32)
-        opcode = le[-7::]
-        func3 = le[length-15:length-12]
-        func7 = le[length-32:length-25]
-        func7_imm = le[length-32:length-27]
-        func7_2bit = le[length-27:length-25]
-        func7_1bit = le[length-28:length-27]
-        func7_fsri_1bit = le[length-27:length-26]
-        func7_imm_SHFL = le[length-32:length-26]
-        imm_value = le[length-25:length-20]
-        imm_value_1 = le[length-25:length-20]
-        fsr_imm_value = le[length-26:length-20]
+        if (opcode == "0110011"): # R-type Instruction
+            if(func7_2bit == "00"): # Normal R-type Instruction
+                le = func7 + le[7:17] + func3_r + le[20:25] + opcode
+            else: #R4-type Instruction
+                le = le[0:5] + func7_2bit + le[7:17] + func3_imm + le[20:25] + opcode
 
-        dut._log.info(f'{le}')
+        if (opcode == "0010011"): # I-type Instruction
+            if(func7_fsri_1bit == "1"): # FSRI Instruction
+                le = le[0:5] + func7_fsri_1bit + le[6:17] + "101" + le[20:25] + opcode
+            else: # Normal I-type Instruction
+                le = func7_imm + "0" + le[6:17] + func3_imm + le[20:25] + opcode
 
         mav_putvalue_instr = int(le,2)
 
@@ -99,5 +100,5 @@ def run_test(dut):
             err_count = err_count + 1
             dut._log.info(error_message)
 
-    final_error_message = f'The behaviour of the DUT differs from that of the model.'
+    final_error_message = f'The behaviour of the DUT differs from that of the model. The DUT fails for {err_count}/{i+1} instructions.'
     assert err_count == 0, final_error_message
